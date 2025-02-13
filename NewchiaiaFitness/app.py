34fad34dd10c23ea5a26f2dd92c7003b91f7adbd
luposian <74679842+luposian@users.noreply.github.com>
2025-02-13 15:13:09 +0100

    app.py

--- NewchiaiaFitness/app.py
+++ NewchiaiaFitness/app.py
@@ -107,4 +107,6 @@ def generate_pdf(data_list, filename="Scheda_Allenamento.pdf", category="General
     return output_path
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
