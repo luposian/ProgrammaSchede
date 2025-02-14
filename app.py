4464b4e352201d26c73624dbd2642fa173c50421
luposian <74679842+luposian@users.noreply.github.com>
2025-02-14 07:03:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Configurazione del database PostgreSQL su Render
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clienti_db_user:vLLLRAV1IVQmKWtj29KV1ckdMJoIZSr8@dpg-cunbbi8gph6c73eq88lg-a.frankfurt-postgres.render.com/clienti_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "DATABASE_URL"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
